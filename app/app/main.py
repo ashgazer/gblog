06e79eddd765389f6748334a7b0450b9704d187a
Ashik Pirmohamed <ashik.pirmohamed@wahanda.com>
2018-05-19 19:43:12 +0100

    added templates

--- app/app/main.py
+++ app/app/main.py
@@ -1,11 +1,24 @@
 from flask import Flask
 app = Flask(__name__)
 
-from app import views
 # @app.route("/")
 # def hello():
 #     return "Hello World from Flask"
 
+
+@app.route('/')
+def index():
+    #return render_template('index.html')
+    return ("damn it")
+
+@app.route('/blog')
+def blog():
+    # return render_template('about.html')
+    return ("some blog bullshit")
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
--- app/app/views.py
+++ app/app/views.py
@@ -3,7 +3,8 @@ from flask import render_template , url_for
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    #return render_template('index.html')
+    return ("damn it")
 
 @app.route('/blog')
 def blog():
