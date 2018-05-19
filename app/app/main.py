226c368aba94d57017e071b2d8ad564c0bd08ca6
Ashik Pirmohamed <ashik.pirmohamed@wahanda.com>
2018-05-19 22:00:47 +0100

    fixed static files rendering

--- app/app/main.py
+++ app/app/main.py
@@ -1,24 +1,25 @@
 from flask import Flask
 app = Flask(__name__)
 
-# @app.route("/")
-# def hello():
-#     return "Hello World from Flask"
+# from core import views
+from  flask import render_template , url_for
 
 
 @app.route('/')
 def index():
-    #return render_template('index.html')
-    return ("damn it")
+    return render_template('index.html')
+    #return ("damn it")
 
 @app.route('/blog')
 def blog():
-    # return render_template('about.html')
-    return ("some blog bullshit")
+    return render_template('about.html')
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+
 if __name__ == "__main__":
     # Only for debugging while developing
     app.run(host='0.0.0.0', debug=True, port=80)
